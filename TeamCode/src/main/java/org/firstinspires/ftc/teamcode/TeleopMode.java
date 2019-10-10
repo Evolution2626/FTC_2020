2c06fc029df147d34b9945bb9441a0d9fc3b90ba
Zach3292 <Zach3292@hotmail.com>
2019-10-10 06:42:07 -0400

    add some comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
@@ -1,22 +1,22 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode;  //Folder
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode; // FTC library
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 
-@TeleOp(name = "Teleop_Test")
+@TeleOp(name = "Teleop_Test")   //Mode
 
-public class TeleopMode extends LinearOpMode {
+public class TeleopMode extends LinearOpMode {  // Basic code here
 
     private Gyroscope imu;
-    private DcMotor frontRight;
+    private DcMotor frontRight;     // Creating all object
     private DcMotor frontLeft;
     private DcMotor rearRight;
     private DcMotor rearLeft;
 
 
-    public void driveTank(double left, double right){
+    public void driveTank(double left, double right){    //Fonction for an easy tank drive(taken from FRC )
         frontRight.setPower(right);
         frontLeft.setPower(left);
         rearRight.setPower(right);
@@ -25,36 +25,36 @@ public class TeleopMode extends LinearOpMode {
 
 
     @Override
-    public void runOpMode() {
+    public void runOpMode() {   //run while init
 
-        imu = hardwareMap.get(Gyroscope.class, "imu");
+        imu = hardwareMap.get(Gyroscope.class, "imu");       // Defining which object is what on the robot
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         rearRight = hardwareMap.get(DcMotor.class, "rearRight");
         rearRight = hardwareMap.get(DcMotor.class, "rearLeft");
 
-        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);   // setting the motor direction
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
         rearRight.setDirection(DcMotor.Direction.FORWARD);
         rearLeft.setDirection(DcMotor.Direction.FORWARD);
 
-        telemetry.addData("Status", "Initialized");
+        telemetry.addData("Status", "Initialized");  // print in console
         telemetry.update();
 
-        waitForStart();
+        waitForStart();  // After run when start
 
-        telemetry.addData("Status", "Running");
+        telemetry.addData("Status", "Running");   // print in console
         telemetry.update();
 
         while (opModeIsActive()) {
 
-            double left_axisY = -this.gamepad1.left_stick_y;
+            double left_axisY = -this.gamepad1.left_stick_y;     //define the joystick variable
             double right_axisY = -this.gamepad1.right_stick_y;
 
-            driveTank(left_axisY, right_axisY);
+            driveTank(left_axisY, right_axisY);    //make the motor move
 
             telemetry.addData("Left", left_axisY);
-            telemetry.addData("Right", right_axisY);
+            telemetry.addData("Right", right_axisY);    // print in the console
             telemetry.addData("Gyro", imu);
             telemetry.update();
         }
